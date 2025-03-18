24c0a54ef81b623bbf5d11023ffe83b8bca91e44
andrewweston <andrew.weston@propellerhead.co.nz>
2025-03-18 15:29:09 +1300

    draft dcql query

--- src/oid4vp/types/query.rs
+++ src/oid4vp/types/query.rs
@@ -3,7 +3,8 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-use crate::oid4vci::types::FormatProfile;
+use crate::oid4vci::types::{CredentialConfiguration, FormatProfile};
+// use crate::oid4vp::Result;
 
 /// DCQL query for requesting Verifiable Presentations.
 #[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
@@ -15,6 +16,7 @@ pub struct DcqlQuery {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub credential_sets: Option<Vec<CredentialSetQuery>>,
 }
+
 /// Represents a request for a presentation of one Credential.
 #[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
 pub struct CredentialQuery {
@@ -25,17 +27,17 @@ pub struct CredentialQuery {
     /// The format of the requested Credential
     pub format: FormatProfile,
 
-    // /// Additional properties requested that apply to the metadata of the
-    // /// Credential. Properties are specific to Credential Format Profile.
-    // #[serde(skip_serializing_if = "Option::is_none")]
-    // pub meta: Option<MetadataQuery>,
-    //
+    /// Additional properties requested that apply to the metadata of the
+    /// Credential. Properties are specific to Credential Format Profile.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub meta: Option<MetadataQuery>,
+
     /// An array of objects that specifies claims in the requested Credential.
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub claims: Option<ClaimsQuery>,
+    pub claims: Option<Vec<ClaimQuery>>,
 
     /// Combinations of claims to use when requesting Credentials. Each set
-    /// consists of one or more `claims` identifiers (`ClaimsQuery.id`).
+    /// consists of one or more `claims` identifiers (i.e. `ClaimsQuery.id`).
     #[serde(skip_serializing_if = "Option::is_none")]
     pub claim_sets: Option<Vec<Vec<String>>>,
 }
@@ -59,7 +61,7 @@ pub struct CredentialSetQuery {
 
 /// Claims entry.
 #[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
-pub struct ClaimsQuery {
+pub struct ClaimQuery {
     /// Identifies the claim within the claims array.
     ///
     /// Required when `claim_sets` is present in the Credential Query.
@@ -67,10 +69,162 @@ pub struct ClaimsQuery {
     pub id: Option<String>,
 
     /// An array of claims path pointers specifying the path to a claim within
-    /// the Verifiable Credential.
+    /// the Credential.
     pub path: Vec<String>,
 
     /// The expected values of the claim.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub values: Option<Vec<Value>>,
+}
+
+impl ClaimQuery {
+    /// Determine whether the specified claim matches the `ClaimQuery`.
+    #[must_use]
+    pub fn matches(&self, claim: &Claim) -> bool {
+        if self.path == claim.path {
+            let Some(values) = &self.values else {
+                return true;
+            };
+            return values.iter().all(|v| claim.values.contains(v));
+        }
+        false
+    }
+}
+
+/// Credential metadata query parameters. Properties are specific to Credential
+/// Format Profile.
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub enum MetadataQuery {
+    /// ISO-MDL format credential metadata.
+    IsoMdl {
+        /// Allowed value for the doctype of the requested Credential.
+        doctype_value: String,
+    },
+
+    /// SD-JWT format credential  metadata.
+    SdJwt {
+        /// Allowed values when querying for SD-JWT Credentials.
+        vct_values: Vec<String>,
+    },
+}
+
+impl MetadataQuery {
+    fn is_match(&self, meta: &CredentialConfiguration) -> bool {
+        match &self {
+            MetadataQuery::IsoMdl { doctype_value } => {
+                if let FormatProfile::IsoMdl { doctype } = &meta.profile {
+                    if doctype != doctype_value {
+                        return false;
+                    }
+                }
+            }
+            MetadataQuery::SdJwt { vct_values } => {
+                if let FormatProfile::DcSdJwt { vct } = &meta.profile {
+                    if !vct_values.contains(vct) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        true
+    }
+}
+
+/// A generic credential claim.
+pub struct Claim {
+    /// The path to the claim within the credential.
+    pub path: Vec<String>,
+
+    /// The claim's values.
+    pub values: Vec<Value>,
+}
+
+/// A trait implemented by credentials so they can be queried by DCQL.
+pub trait Credential {
+    /// Returns the Credential's format.
+    fn meta(&self) -> CredentialConfiguration;
+
+    /// Returns the Credential's claims.
+    fn claims(&self) -> Vec<Claim>;
+}
+
+impl DcqlQuery {
+    /// Determines whether the specified credential matches the query.
+    pub fn is_match(&self, credential: &impl Credential) -> bool {
+        for cq in &self.credentials {
+            if cq.is_match(credential).is_some() {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+impl CredentialQuery {
+    /// Determines whether the specified credential matches the query.
+    pub fn is_match(&self, credential: &impl Credential) -> Option<Vec<Claim>> {
+        // format match
+        if self.format != credential.meta().profile {
+            return None;
+        }
+
+        // metadata match
+        if let Some(meta) = &self.meta {
+            if !meta.is_match(&credential.meta()) {
+                return None;
+            }
+        }
+
+        // claims match
+        let Some(claims) = self.match_claims(credential) else {
+            return None;
+        };
+
+        Some(claims)
+    }
+
+    // Find matching claims in the credential.
+    fn match_claims(&self, credential: &impl Credential) -> Option<Vec<Claim>> {
+        // when no claim queries are specified, return all claims
+        let Some(claim_queries) = &self.claims else {
+            return Some(credential.claims());
+        };
+
+        // when no claim sets are specified, return claims matching claim queries
+        let Some(claim_sets) = &self.claim_sets else {
+            let mut matched = vec![];
+            for claim in credential.claims() {
+                if claim_queries.iter().any(|cq| cq.matches(&claim)) {
+                    matched.push(claim);
+                }
+            }
+            return Some(matched);
+        };
+
+        // return the claims specified by the first matched claim set
+        for claim_set in claim_sets {
+            let mut matched = vec![];
+
+            // find the first matching claim set
+            for id in claim_set {
+                // get claim query by id
+                if let Some(claim_query) =
+                    claim_queries.iter().find(|cq| cq.id.as_ref() == Some(id))
+                {
+                    for claim in credential.claims() {
+                        if claim_query.matches(&claim) {
+                            matched.push(claim);
+                        }
+                    }
+                }
+            }
+
+            if !matched.is_empty() {
+                return Some(matched);
+            }
+        }
+
+        None
+    }
 }
