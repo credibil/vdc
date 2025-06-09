d41510047b7b6358ff327492242e089a86ae25cd
andrewweston <andrew.weston@propellerhead.co.nz>
2025-06-09 19:07:04 +1200

    hack in doc store

--- crates/test-utils/src/identity.rs
+++ crates/test-utils/src/identity.rs
@@ -1,12 +1,10 @@
-use anyhow::{Result, anyhow, bail};
+use anyhow::{Result, anyhow};
 use credibil_identity::did::{self, Document, DocumentBuilder, KeyId, VerificationMethod};
-use credibil_identity::ecc::Algorithm;
-// use test_kms::Keyring;
-use credibil_identity::ecc::{Curve::Ed25519, Keyring, Signer};
+use credibil_identity::ecc::Curve::Ed25519;
+use credibil_identity::ecc::{Algorithm, Keyring, Signer};
 use credibil_identity::jose::PublicKeyJwk;
 use credibil_identity::{Identity, IdentityResolver, Signature, VerifyBy};
 
-use crate::datastore::Store;
 use crate::vault::KeyVault as Vault;
 
 #[derive(Clone)]
@@ -25,15 +23,8 @@ impl DidIdentity {
         // generate a did:web document
         let vm = VerificationMethod::build().key(jwk).key_id(KeyId::Index("key-0".to_string()));
         let builder = DocumentBuilder::new().verification_method(vm).derive_key_agreement(true);
-        let document = did::web::CreateBuilder::new(owner)
-            .document(builder)
-            .build()
-            .expect("should build document");
-        let doc_bytes = serde_json::to_vec(&document).expect("should serialize");
 
-        // save to global datastore
-        let did = did::web::to_did(owner).expect("should create DID");
-        Store::open().put(&did, "DID", &did, &doc_bytes).await.expect("should put");
+        did::web::create(owner, builder).await.expect("should create");
 
         Self {
             owner: owner.to_string(),
@@ -41,12 +32,7 @@ impl DidIdentity {
     }
 
     pub async fn document(&self, url: &str) -> Result<Document> {
-        let url = url.trim_end_matches("/did.json").trim_end_matches("/.well-known");
-        let did = did::web::to_did(url)?;
-        let Some(doc_bytes) = Store::open().get(&did, "DID", &did).await? else {
-            bail!("document not found");
-        };
-        serde_json::from_slice(&doc_bytes).map_err(Into::into)
+        did::web::document(url).await
     }
 }
 
